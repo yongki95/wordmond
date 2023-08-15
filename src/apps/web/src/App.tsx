02da046dd5866f0368ee9a1fc731ec51ad5133f9
Courage kim <yongki95@vt.edu>
2023-08-15 16:20:21 -0700

    Change redundant lines into one line

--- src/apps/web/src/App.tsx
+++ src/apps/web/src/App.tsx
@@ -9,11 +9,7 @@ export default function App() {
       <h2>Wordmond 🚀</h2>
       <br/>
         <div id='level_container'>
-          <DisplayWords page={1} limit={10} level={1}/>
-          <DisplayWords page={1} limit={10} level={2}/>
-          <DisplayWords page={1} limit={10} level={3}/>
-          <DisplayWords page={1} limit={10} level={4}/>
-          <DisplayWords page={1} limit={10} level={5}/>
+        {[...Array(5)].map((_, i) => <DisplayWords key={i} page={1} limit={10} level={i + 1}/>)}
         </div>
     </div>
   );
