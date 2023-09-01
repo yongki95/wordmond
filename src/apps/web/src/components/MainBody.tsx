19dcf6ae7865b4d4cb0f987a477caac4f9278077
Courage kim <yongki95@vt.edu>
2023-08-31 19:52:19 -0700

    Add User Main, Word, About page

--- src/apps/web/src/components/MainBody.tsx
+++ src/apps/web/src/components/MainBody.tsx
@@ -1,16 +1,21 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAnglesLeft, faAnglesRight } from '@fortawesome/free-solid-svg-icons';
 import { FC, useCallback, useMemo } from "react";
-import styled from "styled-components";
+import styled from 'styled-components';
+import { Route, Routes, Router } from 'react-router-dom';
 
-import { TopMain } from './BodyComponents/TopMain';
-import { MiddleMain } from './BodyComponents/MiddleMain';
+import { Home } from '../pages/Main/Main';
+import { About } from '../pages/About/About';
+import { Word } from '../pages/Word/Word';
 
 export const MainBody = () => {
   return (
     <Wrapper>
-        <TopMain/>
-        <MiddleMain/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/Word' element={<Word/>} />
+          <Route path='/About' element={<About/>} />
+        </Routes>
     </Wrapper>
   )
 }
